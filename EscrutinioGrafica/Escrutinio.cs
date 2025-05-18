2000ec4345c353c2e77167ce4af07101c722c25f
xavix45 <xavieranatoa@gmail.com>
2025-05-18 00:21:36 -0500

    Establecer controles de la interfaz gráfica de escrutiño

--- EscrutinioGrafica/Escrutinio.cs
+++ EscrutinioGrafica/Escrutinio.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace EscrutinioGrafica
 {
-    public partial class Form1 : Form
+    public partial class Escrutinio : Form
     {
-        public Form1()
+        public Escrutinio()
         {
             InitializeComponent();
         }
